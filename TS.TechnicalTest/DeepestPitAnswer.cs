b7a1c068443c5f2b82a6933715f06b7fc8e7066f
Thabo Ntsoane <57761034+NtsoaneThabo@users.noreply.github.com>
2025-11-16 14:58:12 +0200

    wip: initial point check and var declaration

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -4,6 +4,19 @@ public class DeepestPitAnswer
 {
     public static int Solution(int[] points)
     {
-        throw new NotImplementedException("Not completed yet");
+        //Get Length of points
+        int totalPoints = points.Length;
+
+        //Make sure there are atleast 3 points
+        if (totalPoints < 3) 
+        {
+            return -1;
+        }
+
+        //no pits found
+        int deepestDepth = -1;
+        int peak = 0;
+
+        return deepestDepth;
     }
 }
