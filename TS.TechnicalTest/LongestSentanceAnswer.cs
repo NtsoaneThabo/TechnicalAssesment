7408c7169e16bc174c31ace91b2cba1fc66c0ab5
Thabo Ntsoane <57761034+NtsoaneThabo@users.noreply.github.com>
2025-11-16 14:27:56 +0200

    wip: Longest Sentence Implementation

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,32 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        
+        //null check
+        if (String.IsNullOrEmpty(s))
+            return 0;
+
+        //Determine sentences
+        string[] sentences = s.Split(new char[] { '.', '?', '!' });
+
+        int maxWordsCount = 0;
+
+        foreach (string sentence in sentences)
+        {
+            Console.WriteLine(sentence);
+            
+            int countWords = 0;
+
+            string[] words = sentence.Split(new char[] { ' ' });
+            foreach (string word in words) 
+                countWords++;
+
+            if (countWords > maxWordsCount)
+            {
+                maxWordsCount = countWords;
+            }
+        }
+
+        return maxWordsCount;
     }
 }
