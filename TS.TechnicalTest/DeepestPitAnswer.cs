af7b71b0cd69c4e41a5bcf6e932d6170c2546174
Thabo Ntsoane <57761034+NtsoaneThabo@users.noreply.github.com>
2025-11-16 15:07:33 +0200

    wip: find peak after ground, pit starts after peak

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -2,6 +2,20 @@
 
 public class DeepestPitAnswer
 {
+
+    /*
+     * A non-empty array A consisting of N integers is given. A pit in this array is any triplet of integers (P, Q, R) such that
+        Each element of array A is an integer within the range [−100,000,000..100,000,000].
+        •
+        Only count the pit if the decline started above ground i.e., 0
+        •
+        Only count it as a pit if the decline is uninterrupted until the lowest point is reached (No flat line)
+        •
+        You stop counting when the water reaches the first ridge
+        •
+        The response must be -1 if there is no pits found in the provided array
+     */
+
     public static int Solution(int[] points)
     {
         //Get Length of points
@@ -15,7 +29,21 @@ public class DeepestPitAnswer
 
         //no pits found
         int deepestDepth = -1;
-        int peak = 0;
+        int peakIndex = 0;
+
+        while (peakIndex < totalPoints - 2)
+        {
+            //find the peak, the pit must start after peak
+            while (peakIndex < totalPoints - 1 && points[peakIndex] <= points[peakIndex + 1])
+                peakIndex++;
+
+            //Peak must be above ground
+            if (points[peakIndex] <= 0)
+            {
+                peakIndex++;
+                continue;
+            }
+        }
 
         return deepestDepth;
     }
